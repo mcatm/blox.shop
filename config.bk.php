78660cb4d92178742f7cc5afb0d8c38a8335d78e
mcatm <tkcs@pelepop.com>
2010-07-22 18:30:58 +0900

    add config file.

---
+++ config.bk.php
@@ -0,0 +1,21 @@
+<?
+
+$config = array(
+	
+);
+
+$admin_menu = array(
+	'top'	=> array(
+		'alias'	=> 'ex/shop'
+	),
+	'item'	=> array(
+		'alias'	=> 'ex/shop/item',
+		'sub'	=> array(
+			'add_item'	=> array(
+				'alias'	=> 'ex/item/add'
+			)
+		)
+	)
+);
+
+?>
